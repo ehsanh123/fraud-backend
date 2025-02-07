a9f0e74d2383921c02b0a1d83c05c8bd6f7dedba
unknown <Elfelf33@yahoo.com>
2025-02-07 19:56:20 +0000

    Initial commit

---
+++ server.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const { execFile } = require('child_process');
+
+const app = express();
+app.use(cors()); // Allow requests from Next.js frontend
+app.use(bodyParser.json());
+
+app.post('/api/submit', (req, res) => {
+    const formData = req.body;
+    const scriptPath = './script.py'; 
+    const pythonPath = "python";
+    p="prediction : 0.996" 
+
+    // return res.status(200).json({ output: p });  // Respond with the output of the script
+    execFile(pythonPath, [scriptPath, JSON.stringify(formData)], (error, stdout, stderr) => {
+
+        if (error) {
+            return res.status(500).json({ error: "Failed to execute script", details: error.message });
+        }
+
+        if (stderr) {
+            return res.status(500).json({ error: "Script error", details: stderr });
+        }
+
+        return res.status(200).json(stdout);
+    });
+});
+
+
+app.get('/api/submit', (req, res) => {
+    const formData = req.body;
+    const scriptPath = './script.py'; 
+    const pythonPath = "python";
+    p={"prediction" : "0.996" }
+
+    return res.status(200).json({ output: "p" });  // Respond with the output of the script
+    // execFile(pythonPath, [scriptPath, JSON.stringify(formData)], (error, stdout, stderr) => {
+    //     if (error) {
+    //         return res.status(500).json({ error: "Failed to execute script", details: error.message });
+    //     }
+    //     if (stderr) {
+    //         return res.status(500).json({ error: "Script error", details: stderr });
+    //     }
+    //     return res.json({ output: stdout });
+    // });
+});
+
+app.get('/', (req, res) => {
+    return res.status(200).json({ output: "Hello World"  });  // Respond with the output of the script
+    
+});
+
+
+app.listen(5000, () => console.log('Backend running on port 5000'));
